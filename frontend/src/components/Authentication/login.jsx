8682bf00cccbc531518e6c3fa038d172169458fd
Sanat Agrawal <sanatagrwl@gmail.com>
2023-09-13 01:46:55 +0530

    frontend

--- frontend/src/components/Authentication/login.jsx
+++ frontend/src/components/Authentication/login.jsx
@@ -1,7 +1,127 @@
-export default function Login() {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
+import React, { useState } from 'react';
+import bg from "./bg.jpg"
+
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+    // Simulate login logic (replace with actual login code)
+    if (email === 'user@example.com' && password === 'password') {
+      setIsLoggedIn(true);
+     
+    } else {
+      setIsLoggedIn(false);
+    }
+  };
+
+  const frameStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    fontFamily: 'Inter, sans-serif',
+    backgroundImage: `url(${bg})`,
+     minHeight: '94.1vh',
+   
+    justifyContent: 'center',
+    backgroundSize: 'cover', // Scale the image to cover the entire container
+    backgroundPosition: 'center', // Center the background image
+    backgroundAttachment: 'fixed', // Fix the background image in place
+  };
+
+  
+
+  
+const imageContainerStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  margin: '20px'
+  
+};
+
+const text = {
+  maxWidth: '100%',
+  maxHeight: '150px',
+  
+  
+
+};
+
+ 
+ 
+const inputStyle = {
+  width: '300px',
+  padding: '10px',
+  margin: '5px 0',
+  border: '1px solid #ccc',
+  borderRadius: '5px',
+  fontSize: '16px',
+ 
+  
+};
+
+  const buttonStyle = {
+    margin:'15px',
+    width: '150px',
+    padding: '12px',
+    backgroundColor: '#3D52D5',
+    border: 'none',
+    borderRadius: '5px',
+    color: '#FFF',
+    fontSize: '16px',
+    cursor: 'pointer',
+  }
+  
+  const anchorStyle = {
+    color: '#fff', // Adjust the color as needed
+    textDecoration: 'none', // Remove underlines
+    fontSize: '20px', // Adjust the font size
+    margin: '5px', // Adjust the spacing
+  };
+  
+
+  return (
+    <div style={frameStyle}>
+      
+
+      <div style={imageContainerStyle}>
+        <h1 style={text}>LOGIN</h1>
+      </div>
+        
+          <input
+            type="text"
+            style={inputStyle}
+            placeholder="Username"
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <input
+            type="password"
+            style={inputStyle}
+            placeholder="Password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <button style={buttonStyle} onClick={handleLogin}>
+            LOGIN
+          </button>
+          <a href='#' style={anchorStyle}>Forgot password?</a>
+          {isLoggedIn && <p>Logged in successfully!</p>}
+          
+       
+      
+    </div>
+  );
 }
+
+export default Login;
--- frontend/src/components/Authentication/signup.jsx
+++ frontend/src/components/Authentication/signup.jsx
@@ -1,7 +1,176 @@
-export default function Signup(){
-    return(
-        <div>
-            <h1>Signup</h1>
-        </div>
-    )
+import React, { useState } from 'react';
+import bg from "./bg.jpg"
+
+
+
+const inputStyle = {
+  width: '300px',
+  padding: '10px',
+  margin: '5px 0',
+  border: '1px solid #ccc',
+  borderRadius: '5px',
+  fontSize: '16px',
+  backgroundColor: '#fff' ,
+  
+};
+const inputStyle1 = {
+  width: '320px',
+  padding: '10px',
+  margin: '5px 0',
+  border: '1px solid #ccc',
+  borderRadius: '5px',
+  fontSize: '16px',
+  backgroundColor: '#fff' ,
+  
+};
+
+
+const buttonStyle = {
+  margin: '10px',
+  width: '150px',
+  padding: '12px',
+  backgroundColor: '#3D52D5',
+  border: 'none',
+  borderRadius: '5px',
+  color: '#FFF',
+  fontSize: '16px',
+  cursor: 'pointer',
+  opacity: 1, // Set initial opacity to 1 (fully opaque)
+};
+
+const disabledButtonStyle = {
+  ...buttonStyle,
+  opacity: 0.5, // Lower opacity when button is disabled
+  cursor: 'not-allowed', // Change cursor to indicate button is disabled
+};
+
+
+
+
+
+function Signup() {
+  const departments = ["Department 1", "Department 2", "Department 3"]; // Add your department options
+  const degrees = ["Degree 1", "Degree 2", "Degree 3"]; // Add your degree options
+  const [email, setEmail] = useState('');
+  const [department, setDepartment] = useState('');
+  const [name, setName] = useState('');
+  const [degree, setDegree] = useState('');
+  const [contactNumber, setContactNumber] = useState('');
+  const [password, setPassword] = useState('');
+  const [rollNumber, setRollNumber] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  
+  const [passwordMatch, setPasswordMatch] = useState(false);
+
+  const handleConfirmPasswordChange = (event) => {
+    const confirmPasswordValue = event.target.value;
+    setConfirmPassword(confirmPasswordValue);
+    setPasswordMatch(password === confirmPasswordValue);
+    
+  };
+
+  const isValidEmail = (email) => {
+    // Use a regular expression for basic email validation
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  };
+
+  
+  const handleEmailChange = (event) => setEmail(event.target.value);
+  const handleDepartmentChange = (event) => setDepartment(event.target.value);
+  const handleNameChange = (event) => setName(event.target.value);
+  const handleDegreeChange = (event) => setDegree(event.target.value);
+  const handleContactNumberChange = (event) => setContactNumber(event.target.value);
+  const handlePasswordChange = (event) => setPassword(event.target.value);
+  const handleRollNumberChange = (event) => setRollNumber(event.target.value);
+
+  const allFieldsFilled = () => {
+    return (
+      isValidEmail(email) &&
+      department !== "" &&
+      degree !== "" &&
+      name !== "" &&
+      contactNumber !== "" &&
+      password !== "" &&
+      rollNumber !== "" &&
+      confirmPassword !== "" &&
+      passwordMatch
+    );
+  };
+   
+
+  const handleRegistration = () => {
+    // Simulate registration logic (replace with actual registration code)
+  
+  };
+
+  const formStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    fontFamily: 'Inter, sans-serif',
+    backgroundImage: `url(${bg})`,
+    minHeight: '94.1vh',
+    justifyContent: 'center',
+    backgroundSize: 'cover', // Scale the image to cover the entire container
+    backgroundPosition: 'center', // Center the background image
+    backgroundAttachment: 'fixed', // Fix the background image in place
+    
+  };
+
+  const headingStyle = {
+    fontSize: '26px',
+    marginBottom: '20px',
+  };
+
+  return (
+
+    <div style={formStyle}>
+      
+      <h1 style={headingStyle}>REGISTER</h1>
+      <input type="text" placeholder="Name" value={name} onChange={handleNameChange} style={inputStyle} required />
+      <input type="email" placeholder="Email ID" value={email} onChange={handleEmailChange}  required  style={{
+          ...inputStyle,
+          border: (isValidEmail(email) || email==='') ? '' : '2px solid red'
+        }} />
+
+      <input type="text" placeholder="Contact Number" value={contactNumber} onChange={handleContactNumberChange} style={inputStyle} />
+      <select value={department} onChange={handleDepartmentChange} style={inputStyle1} required>
+        <option value="" disabled>Select Department</option>
+        {departments.map((dept, index) => (
+          <option key={index} value={dept}>{dept}</option>
+        ))}
+      </select>
+      <select value={degree} onChange={handleDegreeChange} style={inputStyle1}>
+        <option value="" disabled>Select Degree</option>
+        {degrees.map((deg, index) => (
+          <option key={index} value={deg}>{deg}</option>
+        ))}
+      </select>
+      
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={handlePasswordChange}
+        style={inputStyle}
+      />
+  <input
+        type="password"
+        placeholder="Confirm Password"
+        value={confirmPassword}
+        onChange={handleConfirmPasswordChange}
+        style={inputStyle}
+      />
+      
+      <input type="text" placeholder="Roll Number" value={rollNumber} onChange={handleRollNumberChange} style={inputStyle} />
+      <button onClick={handleRegistration} style={allFieldsFilled() ? buttonStyle : disabledButtonStyle}
+        disabled={!allFieldsFilled()}   >REGISTER</button>
+      
+    </div>
+  );
 }
+
+
+
+export default Signup;
