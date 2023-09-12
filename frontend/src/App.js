3e4705a9dfe1a1b03a7f306196f184361079ac9e
Akash Banger <akashbanger2@gmail.com>
2023-09-12 10:48:02 +0530

    routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
-
-// Import your components for different pages
-import Home from './components/Home';
-import About from './components/About';
-import Team from './components/Team';
-import Gallery from './components/gallery';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Switch,
+  Link,
+} from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./components/Home/Home";
+import About from "./components/Home/About";
+import Team from "./components/Home/Team";
+import Gallery from "./components/Home/Gallery";
+import Signup from "./components/Authentication/signup";
+import Login from "./components/Authentication/login";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <nav>
-            {/* Create navigation links */}
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/team">Team</Link>
-              </li>
-              <li>
-                <Link to="/gallery">Gallery</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/team">
-            <Team />
-          </Route>
-          <Route path="/gallery">
-            <Gallery />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
-
---
+++ frontend/src/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+    return (
+        <div style={{ display: 'flex', flexDirection: "row" }}>
+            <a style={{ margin: "10px" }} href="/">Home</a>
+            <a style={{ margin: "10px" }} href="/about">About</a>
+            <a style={{ margin: "10px" }} href="/team">Team</a>
+            <a style={{ margin: "10px" }} href="/gallery">Gallery</a>
+            <a style={{ margin: "10px" }} href="/login">Login</a>
+            <a style={{ margin: "10px" }} href="/signup">Signup</a>
+        </div>
+    )
+}
