805b80255ccba6d095aec334e85bf1c8082e538a
pratiksha <pratikshajjn045@gmail.com>
2024-09-24 01:23:06 +0530

    newhomesetup

---
+++ frontend/src/components/Home/Home1.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Home1.css";
+
+function Home1(){
+ return(
+    <div className='newhome'>
+        <div className='Landingpage'>
+            Alumination
+        </div>
+        <div className='About'>
+            about
+        </div>
+    </div>
+ )
+
+};
+export default Home1;
