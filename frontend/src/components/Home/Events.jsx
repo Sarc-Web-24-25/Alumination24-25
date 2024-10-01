5ecc911df472619d8dbc7508d779fcac6f81e571
Abdiitb <aryan223653badkul@gmail.com>
2024-10-02 02:38:39 +0530

    events page + footer animation issue

--- frontend/src/components/Home/Events.jsx
+++ frontend/src/components/Home/Events.jsx
@@ -45,15 +45,33 @@ function EventList() {
             });
     }, []);
 
-    const handleClick = (index) => {
+    const handleClick = (index, title, desc) => {
+        const div = document.createElement('div');
+        const divParent = document.querySelector('#events' + `${index}`);
+
+        div.innerHTML = `
+            <h3 class="event-title">${title}</h3>
+            <p class="event-description">${desc}</p> <br />
+        `
+
+        div.className = "event-info" + " " + (index%2 === 0 ? 'border-left' : 'border-right')
+
+        div.style.paddingRight = "20px";
+        div.style.paddingLeft = "20px";
+        
+        divRefs.current[index] = div;
+
+
+        divParent.append(div);
+
         if(divRefs.current[index]){
             gsap.from(divRefs.current[index], {
-            x: (index%2 === 0) ? 300 : -300, // Slide out by 300px (adjust based on your needs)
-            duration: 1, // Animation duration in seconds
+            x: (index%2 === 0) ? -300 : 300, // Slide out by 300px (adjust based on your needs)
+            duration: 2, // Animation duration in seconds
             ease: 'power3.out', // Easing function for smoothness
             opacity: 0
         });
-    }
+        }
       };
 
     //   const handleClickRight = () => {
@@ -68,24 +86,26 @@ function EventList() {
     return (
         <div className="event-list-container">
             {/*<CursorAnimation />*/}
-            <div className="top-section">
-                <img src={topImage} alt="Top Image" className="top-image" />
+            {/* <div className="top-section">
+                <img src={topImage} alt="Top Image" className="top-image"/>
                 <h1 className="top-title">EVENTS</h1>
-            </div>
+            </div> */}
             <div className="page-content">
                 <ul>
                 
                     {events.map((event, index) => (
                         
                         <li key={event.id} style={{flexDirection: index%2 !== 0 && "row-reverse"}} className="event-list-item">
-                            <div className="event-image" onClick={() => handleClick(index)}>
-                                <img style={{borderRadius:"20px"}} src={img} alt={event.name} width={"100%"} />
+                            <div className="event-image" onClick={() => handleClick(index, event.name, event.description)}>
+                                <img style={{borderRadius:"20px"}} src={img} alt={event.name} />
                             </div>
-                            <div style={{paddingLeft: index%2!==0 && "20px", paddingRight: index%2===0 && "20px"}} className="event-info" ref={(el) => (divRefs.current[index] = el)}>
-                                <h3 className="event-title">{event.name}</h3>
-                                <p className="event-description">{event.description}</p> <br />
-                                {/* <a style={{float: index%2 === 0 && "right", marginRight: index%2 === 0 && "20px"}} href={`/${event.id}`}><button style={{marginTop: "-20px"}} className="register-button">Know More</button></a> */}
-                            </div>
+                            <div id={'events' + index} style={{width: '100%'}}></div>
+                            {/* <div style={{paddingLeft: index%2!==0 && "20px", paddingRight: index%2===0 && "20px"}} className={"event-info" + " " + (index%2 === 0 ? 'border-left' : 'border-right')} ref={(el) => (divRefs.current[index] = el)}>
+                                <h3 className="event-title"></h3>
+                                <p className="event-description"></p> <br />
+                                <a style={{float: index%2 === 0 && "right", marginRight: index%2 === 0 && "20px"}} href={`/${event.id}`}><button style={{marginTop: "-20px"}} className="register-button">Know More</button></a>
+                            </div> */}
+                            
                         </li>
                     ))}
                 </ul>
