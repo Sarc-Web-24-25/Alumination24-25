5f6f6b52ee803c15c34b017ca8e106927083fcda
Kaushik035 <kaushikmandal629@gmail.com>
2024-10-02 16:17:00 +0530

    schedule page ongoing

--- frontend/src/components/Home/schedule.jsx
+++ frontend/src/components/Home/schedule.jsx
@@ -1,16 +1,50 @@
-import React, { useState } from 'react';
-import './schedule.css'; // Import your CSS file
-import CursorAnimation from "./CursorAnimation"
+import React, { useState } from "react";
+import "./schedule.css"; // Import your CSS file
+import CursorAnimation from "./CursorAnimation";
 function Schedule() {
-    
-
-   
-
-    
-
-    return (
-        <div className="schedule_container"></div>
-    );
+  return (
+    <div className="schedule_container">
+      <div className="schedule_upper">SCHEDULE</div>
+      <div className="schedule_lower">
+        <div className="schedule_item1">
+          <div className="schedule_event1">Events</div>
+          <div className="schedule_time1">Time</div>
+        </div>
+        <div className="schedule_item">
+          <div className="schedule_event"></div>
+          <div className="schedule_time"></div>
+        </div>
+        <div className="schedule_item">
+          <div className="schedule_event"></div>
+          <div className="schedule_time"></div>
+        </div>
+        <div className="schedule_item">
+          <div className="schedule_event"></div>
+          <div className="schedule_time"></div>
+        </div>
+        <div className="schedule_item">
+          <div className="schedule_event"></div>
+          <div className="schedule_time"></div>
+        </div>
+        <div className="schedule_item">
+          <div className="schedule_event"></div>
+          <div className="schedule_time"></div>
+        </div>
+        <div className="schedule_item">
+          <div className="schedule_event"></div>
+          <div className="schedule_time"></div>
+        </div>
+        <div className="schedule_item">
+          <div className="schedule_event"></div>
+          <div className="schedule_time"></div>
+        </div>
+        <div className="schedule_item">
+          <div className="schedule_event"></div>
+          <div className="schedule_time"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Schedule;
