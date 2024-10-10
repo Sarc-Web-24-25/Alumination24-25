7e7a1552d1f9724b8f05f7c9171750b49176a939
Arush <arrush.narayan01@gmail.com>
2024-10-10 19:30:15 +0530

    event heading

--- frontend/src/components/Home/EventCarousel.jsx
+++ frontend/src/components/Home/EventCarousel.jsx
@@ -1,27 +1,37 @@
-import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import React from "react";
+import { Carousel } from "react-bootstrap";
 // import image from './bgimg/alumination/image2.JPG'
 
-const ImageCarousel = ( {main} ) => {
+const ImageCarousel = ({ main }) => {
   return (
-    <Carousel className='container' indicators={false} slide={true} style={{height: 'auto', marginBottom: '10vh'}}>
-       
-        {main.map((event) => (
-            // console.log('http://127.0.0.1:8000/' + event.image);
-            <Carousel.Item style={{margin: '20vh 0vh'}}>
-            <img
-               className="d-block w-100"
-               src={'http://127.0.0.1:8000/' + event.image}
-               alt="First slide"
-               style={{width: '500px', height: '500px', borderRadius: '10px'}}
-            />
-            <Carousel.Caption>
-                <h3 style={{fontFamily: `"Kadwa", serif`}}>{event.name}</h3>
-               <p style={{fontFamily: `"Kadwa", serif`, fontFamily: `"Sansita", sans-serif`}}>{event.description}</p>
-            </Carousel.Caption>
-            </Carousel.Item>
-        ))}
-     
+    <Carousel
+      className="container"
+      indicators={false}
+      slide={true}
+      style={{ height: "auto", marginBottom: "10vh" }}
+    >
+      {main.map((event) => (
+        // console.log('http://127.0.0.1:8000/' + event.image);
+        <Carousel.Item style={{ margin: "20vh 0vh" }}>
+          <img
+            className="d-block w-100"
+            src={"http://127.0.0.1:8000/" + event.image}
+            alt="First slide"
+            style={{ width: "500px", height: "500px", borderRadius: "10px" }}
+          />
+          <Carousel.Caption>
+            <h3 style={{ fontFamily: `"Kadwa", serif` }}>{event.name}</h3>
+            <p
+              style={{
+                fontFamily: `"Kadwa", serif`,
+                fontFamily: `"Sansita", sans-serif`,
+              }}
+            >
+              {event.description}
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
 
       {/* <Carousel.Item>
         <img
