61202144f28ab74cef841a80992b55b00dbdd55c
Arush <arrush.narayan01@gmail.com>
2024-10-10 15:34:50 +0530

    about us heading css

--- frontend/src/components/Home/Home1.jsx
+++ frontend/src/components/Home/Home1.jsx
@@ -1,15 +1,14 @@
-
-import React, { useState, useRef, useEffect,} from "react";
+import React, { useState, useRef, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Parallax } from "react-parallax";
 import "./Home1.css";
 import "./Petal.css";
 import layer1 from "./photos24/layer1.png";
-import layer1PH from "./photos24/parallaxLandingPH.jpg"
+import layer1PH from "./photos24/parallaxLandingPH.jpg";
 import layer2 from "./photos24/layer2.png";
 import layer3 from "./photos24/layer3.png";
-import layer3PH from "./photos24/coinParallaxPH.jpg"
+import layer3PH from "./photos24/coinParallaxPH.jpg";
 import layer4 from "./photos24/layer4.png"; // New layer
 import layer5 from "./photos24/layer5.png"; // New layer
 import dragon from "./photos24/dragon.png";
@@ -24,9 +23,7 @@ import Character from "./Character";
 import Alumni3 from "./Alumni3/Alumni3.jsx";
 import Sponsor2 from "./Sponsor2/Sponsor2.jsx";
 import Footer from "./Footer.jsx";
-import Aluminatiom from './photos24/AluminationLogo.png'
-
-
+import Aluminatiom from "./photos24/AluminationLogo.png";
 
 function Home1() {
   const [isMuted, setIsMuted] = useState(false);
@@ -35,77 +32,81 @@ function Home1() {
   const audioRef = useRef(new Audio(backgroundMusic));
 
   // Scroll ref for parallax sections
-const layerRefs = [useRef(), useRef(), useRef(), useRef(), useRef()];
-const previousScrollY = useRef(0); // To track the previous scroll position
+  const layerRefs = [useRef(), useRef(), useRef(), useRef(), useRef()];
+  const previousScrollY = useRef(0); // To track the previous scroll position
 
-// useEffect(() => {
-//   const handleScroll = () => {
-//     const currentScrollY = window.scrollY;
-//     const direction = currentScrollY > previousScrollY.current ? "down" : "up"; // Determine scroll direction
-//     previousScrollY.current = currentScrollY;
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const currentScrollY = window.scrollY;
+  //     const direction = currentScrollY > previousScrollY.current ? "down" : "up"; // Determine scroll direction
+  //     previousScrollY.current = currentScrollY;
 
-//     for (let i = 0; i < layerRefs.length; i++) {
-//       const layer = layerRefs[i].current;
-//       const layerPosition = layer.getBoundingClientRect().top;
-//       const layerHeight = layer.offsetHeight;
+  //     for (let i = 0; i < layerRefs.length; i++) {
+  //       const layer = layerRefs[i].current;
+  //       const layerPosition = layer.getBoundingClientRect().top;
+  //       const layerHeight = layer.offsetHeight;
 
-//       // Check if 10% of the layer is visible
-//       const isLayerVisible = layerPosition <= window.innerHeight * 0.9 && layerPosition >= -layerHeight * 0.1;
+  //       // Check if 10% of the layer is visible
+  //       const isLayerVisible = layerPosition <= window.innerHeight * 0.9 && layerPosition >= -layerHeight * 0.1;
 
-//       // If scrolling down and the layer below is visible, or scrolling up and the layer above is visible
-//       if (isLayerVisible && ((direction === "down" && layerPosition >= 0) || (direction === "up" && layerPosition <= 0))) {
-//         window.scrollTo({
-//           top: window.scrollY + layerPosition,
-//           behavior: "smooth",
-//         });
-//         break;
-//       }
-//     }
-//   };
+  //       // If scrolling down and the layer below is visible, or scrolling up and the layer above is visible
+  //       if (isLayerVisible && ((direction === "down" && layerPosition >= 0) || (direction === "up" && layerPosition <= 0))) {
+  //         window.scrollTo({
+  //           top: window.scrollY + layerPosition,
+  //           behavior: "smooth",
+  //         });
+  //         break;
+  //       }
+  //     }
+  //   };
 
-//   window.addEventListener("scroll", handleScroll);
-//   return () => window.removeEventListener("scroll", handleScroll);
-// }, []);
+  //   window.addEventListener("scroll", handleScroll);
+  //   return () => window.removeEventListener("scroll", handleScroll);
+  // }, []);
 
-useEffect(() => {
-  const debounce = (func, wait) => {
-    let timeout;
-    return (...args) => {
-      const later = () => {
+  useEffect(() => {
+    const debounce = (func, wait) => {
+      let timeout;
+      return (...args) => {
+        const later = () => {
+          clearTimeout(timeout);
+          func(...args);
+        };
         clearTimeout(timeout);
-        func(...args);
+        timeout = setTimeout(later, wait);
       };
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
     };
-  };
 
-  const handleScroll = debounce(() => {
-    const currentScrollY = window.scrollY;
-    const direction = currentScrollY > previousScrollY.current ? "down" : "up";
-    previousScrollY.current = currentScrollY;
+    const handleScroll = debounce(() => {
+      const currentScrollY = window.scrollY;
+      const direction = currentScrollY > previousScrollY.current ? "down" : "up";
+      previousScrollY.current = currentScrollY;
 
-    for (let i = 0; i < layerRefs.length; i++) {
-      const layer = layerRefs[i].current;
-      const layerPosition = layer.getBoundingClientRect().top;
-      const layerHeight = layer.offsetHeight;
+      for (let i = 0; i < layerRefs.length; i++) {
+        const layer = layerRefs[i].current;
+        const layerPosition = layer.getBoundingClientRect().top;
+        const layerHeight = layer.offsetHeight;
 
-      const isLayerVisible = layerPosition <= window.innerHeight * 0.95 && layerPosition >= -layerHeight * 0.05;
+        // Check if 25% of the layer is visible
+        const isLayerVisible = layerPosition <= window.innerHeight * 0.75 && layerPosition >= -layerHeight * 0.25;
 
-      if (isLayerVisible && ((direction === "down" && layerPosition >= 0) || (direction === "up" && layerPosition <= 0))) {
-        window.scrollTo({
-          top: window.scrollY + layerPosition,
-          behavior: "smooth",
-        });
-        break;
+        if (
+          isLayerVisible &&
+          ((direction === "down" && layerPosition >= 0) ||
+            (direction === "up" && layerPosition <= 0))
+        ) {
+          window.scrollTo({
+            top: window.scrollY + layerPosition,
+            behavior: "smooth",
+          });
+          break;
+        }
       }
-    }
-  }, 20); // Adjust the delay to 100ms for debouncing
+    }, 20); // Adjust the delay to 100ms for debouncing
 
-  window.addEventListener("scroll", handleScroll);
-  return () => window.removeEventListener("scroll", handleScroll);
-}, []);
-
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   useEffect(() => {
     const handleEnded = () => {
@@ -116,8 +117,6 @@ useEffect(() => {
         });
       }
     };
-
-    
 
     audioRef.current.addEventListener("ended", handleEnded);
 
@@ -166,39 +165,37 @@ useEffect(() => {
   const paragraph =
     "A Student run organisation at IIT Bombay, Connecting 60k+ Alumni and 12k+ Students Actively strengthens Student alumni relations through robust calendar of 50+ events conducted throughout the year. Student Alumni Relations Cell has been proudly fostering a vibrant student - alumni Community since 2008.";
 
-    const navigate = useNavigate(); // Initialize the navigate function
+  const navigate = useNavigate(); // Initialize the navigate function
 
-    const handleNavClick = (route) => {
+  const handleNavClick = (route) => {
+    navigate(route); // Navigate to the specified route
+  };
 
-      navigate(route); // Navigate to the specified route
+  const [bgImage1, setBgImage1] = useState(layer1); // Default background image
+  const [bgImage3, setBgImage3] = useState(layer3); // Default background image
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 786) {
+        setBgImage1(layer1PH); // Use small image for screens smaller than 786px
+        setBgImage3(layer3PH); // Use small image for screens smaller than 786px
+      } else {
+        setBgImage1(layer1); // Use default image for larger screens
+        setBgImage3(layer3); // Use default image for larger screens
+      }
     };
 
+    // Call the function on initial load
+    handleResize();
 
-    const [bgImage1, setBgImage1] = useState(layer1); // Default background image
-    const [bgImage3, setBgImage3] = useState(layer3); // Default background image
+    // Add event listener to handle resize
+    window.addEventListener("resize", handleResize);
 
-    useEffect(() => {
-      const handleResize = () => {
-        if (window.innerWidth < 786) {
-          setBgImage1(layer1PH); // Use small image for screens smaller than 786px
-          setBgImage3(layer3PH); // Use small image for screens smaller than 786px
-        } else {
-          setBgImage1(layer1); // Use default image for larger screens
-          setBgImage3(layer3); // Use default image for larger screens
-        }
-      };
-  
-      // Call the function on initial load
-      handleResize();
-  
-      // Add event listener to handle resize
-      window.addEventListener("resize", handleResize);
-  
-      // Cleanup event listener on component unmount
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    }, []);
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div className="newhome">
@@ -232,7 +229,6 @@ useEffect(() => {
 
       {/* Parallax Layer 1 */}
       <Parallax bgImage={bgImage1} strength={200}>
-
         <div
           className={`mainHome ${isLayer2Visible ? "moveDown" : ""}`}
           style={{ height: "100vh", position: "relative" }}
@@ -259,10 +255,7 @@ useEffect(() => {
           {petals}
 
           <div className="upperMainHome">
-            <div
-              className="headingHome"
-        
-            >
+            <div className="headingHome">
               <img src={Aluminatiom} alt="" />
               <div className="noren">Noren of Nostalgia</div>
             </div>
@@ -272,57 +265,52 @@ useEffect(() => {
             <div
               className="registerHome"
               style={{ fontSize: "25px", color: "#700815" }}
-              onClick={() => handleNavClick( '/signup')}
+              onClick={() => handleNavClick("/signup")}
             >
               REGISTER
             </div>
           </div>
         </div>
-
-
       </Parallax>
 
-      <div className="clouds1" >
-          <img src={cloud1} alt="cloud1" className="cloud" />
-          <img src={cloud2} alt="cloud2" className="cloud" />
-          <img src={cloud3} alt="cloud3" className="cloud" />
-          {/* <img src={cloud2} alt="cloud3" className="cloud" /> */}
-          {/* <img src={cloud3} alt="cloud3" className="cloud" />
+      <div className="clouds1">
+        <img src={cloud1} alt="cloud1" className="cloud" />
+        <img src={cloud2} alt="cloud2" className="cloud" />
+        <img src={cloud3} alt="cloud3" className="cloud" />
+        {/* <img src={cloud2} alt="cloud3" className="cloud" /> */}
+        {/* <img src={cloud3} alt="cloud3" className="cloud" />
           <img src={cloud1} alt="cloud1" className="cloud" />
           <img src={cloud3} alt="cloud3" className="cloud" /> */}
-        </div>
+      </div>
 
       {/* Parallax Layer 2 */}
       <Parallax bgImage={layer2} strength={200} zIndex={1000}>
-        <div className="layer2" style={{ height: "100vh"}}
-        ref={layerRefs[1]}>
+        <div className="layer2" style={{ height: "150vh" }} ref={layerRefs[1]}>
           {/* <h1 style={{ textAlign: "center", color: "#fff" }}>Layer 2 Content</h1> */}
 
           {/* About Us Section */}
-          <div className="About" >
-            <div className="Abouth">
-            <p style={{ fontSize: "150%" }}>About us</p> 
+          <div className="About">
+            <div className="About_Heading">
+              <p>About us</p>
             </div>
-            <div className="Aboutp">
-            <Character paragraph={paragraph} />
+            <div className="About_Paragraph">
+              <Character paragraph={paragraph} />
             </div>
           </div>
         </div>
 
         {/* Clouds above Layer 2 */}
-
       </Parallax>
 
-
-      <div className="clouds2" >
-          <img src={cloud1} alt="cloud1" className="cloud" />
-          <img src={cloud2} alt="cloud2" className="cloud" />
-          <img src={cloud3} alt="cloud3" className="cloud" />
-          {/* <img src={cloud2} alt="cloud3" className="cloud" /> */}
-          {/* <img src={cloud3} alt="cloud3" className="cloud" />
+      <div className="clouds2">
+        <img src={cloud1} alt="cloud1" className="cloud" />
+        <img src={cloud2} alt="cloud2" className="cloud" />
+        <img src={cloud3} alt="cloud3" className="cloud" />
+        {/* <img src={cloud2} alt="cloud3" className="cloud" /> */}
+        {/* <img src={cloud3} alt="cloud3" className="cloud" />
           <img src={cloud1} alt="cloud1" className="cloud" />
           <img src={cloud3} alt="cloud3" className="cloud" /> */}
-        </div>
+      </div>
 
       {/* Parallax Layer 3 */}
       <Parallax bgImage={bgImage3} strength={150}>
@@ -330,58 +318,56 @@ useEffect(() => {
           {/* <h1 style={{ textAlign: "center", color: "#fff" }}>
             Layer 3 Content
           </h1> */}
-          
+
           <Count />
         </div>
-
       </Parallax>
 
-      <div className="clouds3" >
-          <img src={cloud1} alt="cloud1" className="cloud" />
-          <img src={cloud2} alt="cloud2" className="cloud" />
-          <img src={cloud3} alt="cloud3" className="cloud" />
-          {/* <img src={cloud2} alt="cloud3" className="cloud" /> */}
-          {/* <img src={cloud3} alt="cloud3" className="cloud" />
+      <div className="clouds3">
+        <img src={cloud1} alt="cloud1" className="cloud" />
+        <img src={cloud2} alt="cloud2" className="cloud" />
+        <img src={cloud3} alt="cloud3" className="cloud" />
+        {/* <img src={cloud2} alt="cloud3" className="cloud" /> */}
+        {/* <img src={cloud3} alt="cloud3" className="cloud" />
           <img src={cloud1} alt="cloud1" className="cloud" />
           <img src={cloud3} alt="cloud3" className="cloud" /> */}
-        </div>
+      </div>
 
       {/* Parallax Layer 4 */}
       <Parallax bgImage={layer4} strength={100}>
-        <div className="layer4" style={{ height: "fit-content" }}
-        ref={layerRefs[3]}>
-          <div style={{height: "20vh"}}></div>
+        <div
+          className="layer4"
+          style={{ height: "fit-content" }}
+          ref={layerRefs[3]}
+        >
+          <div style={{ height: "20vh" }}></div>
           <Alumni3 />
-          <div style={{height: "10vh"}}></div>
+          <div style={{ height: "10vh" }}></div>
         </div>
-
       </Parallax>
 
-      <div className="clouds4" >
-          <img src={cloud1} alt="cloud1" className="cloud" />
-          <img src={cloud2} alt="cloud2" className="cloud" />
-          <img src={cloud3} alt="cloud3" className="cloud" />
-          {/* <img src={cloud2} alt="cloud3" className="cloud" /> */}
-          {/* <img src={cloud3} alt="cloud3" className="cloud" />
+      <div className="clouds4">
+        <img src={cloud1} alt="cloud1" className="cloud" />
+        <img src={cloud2} alt="cloud2" className="cloud" />
+        <img src={cloud3} alt="cloud3" className="cloud" />
+        {/* <img src={cloud2} alt="cloud3" className="cloud" /> */}
+        {/* <img src={cloud3} alt="cloud3" className="cloud" />
           <img src={cloud1} alt="cloud1" className="cloud" />
           <img src={cloud3} alt="cloud3" className="cloud" /> */}
-        </div>
+      </div>
 
       {/* Parallax Layer 5 */}
       <Parallax bgImage={layer5} strength={50}>
         <div className="layer5" style={{ height: "100vh" }} ref={layerRefs[4]}>
-        <Sponsor2 sponsors={sponsors}/>
+          <Sponsor2 sponsors={sponsors} />
         </div>
         <Footer></Footer>
       </Parallax>
-            
     </div>
   );
 }
 
 export default Home1;
-
-
 
 // "use client";
 
