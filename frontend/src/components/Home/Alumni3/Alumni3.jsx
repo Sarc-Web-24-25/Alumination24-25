d07d5d7ad6d985a4d5e5ccaffc95c3ce08f79ed8
Arush <arrush.narayan01@gmail.com>
2024-10-12 01:31:45 +0530

    past speakers photo

--- frontend/src/components/Home/Alumni3/Alumni3.jsx
+++ frontend/src/components/Home/Alumni3/Alumni3.jsx
@@ -4,22 +4,22 @@ import japanDaisukiFont from '../../../assets/fonts/JapanDaisuki-8OeaZ.woff'
 import Nandan_Nilekani from '../photos24/Nandan_Nilekani.png'
 import Bhavish_Aggarwal from '../photos24/Bhavish_Aggarwal.png'
 import Nitesh_Tiwari from '../photos24/Nitesh_Tiwari.png'
-import Sameer_Saxena from '../photos24/Sameer_Saxena.jpg'
-import Gaganjeet_Singh from '../photos24/Gaganjeet_Singh.jpeg'
-import Ashima_Mittal from '../photos24/Ashima_Mittal.jpeg'
-import Vipul_Goyal from '../photos24/Vipul_Goyal.jpg'
-import Shubham_Kumar from '../photos24/Shubham_Kumar.jpeg'
-import Ahana_Gautam from '../photos24/Ahana_Gautam.jpeg'
+import Sameer_Saxena from '../photos24/Sameer_Saxena.png'
+import Gaganjeet_Singh from '../photos24/Gaganjeet_Singh.png'
+import Ashima_Mittal from '../photos24/Ashima_Mittal.png'
+import Vipul_Goyal from '../photos24/Vipul_Goyal.png'
+import Shubham_Kumar from '../photos24/Shubham_Kumar.png'
+import Ahana_Gautam from '../photos24/Ahana_Gautam.png'
 
 const alumniList = [  
   {
     name: "Nandan_Nilekani",
-    designation: "Co-Founder Infosys",
+    designation: "Co-Founder, Infosys",
     imageSrc: Nandan_Nilekani,
   },
   {
     name: "Bhavish Aggarwal",
-    designation: "CEO Ola Cabs",
+    designation: "CEO, Ola Cabs",
     imageSrc: Bhavish_Aggarwal,
   },
   {
@@ -34,7 +34,7 @@ const alumniList = [
   },
   {
     name: "Ashima Mittal",
-    designation: "Indian Administrative Services ‘18",
+    designation: "Indian Administrative Services '18",
     imageSrc: Ashima_Mittal,
   },
   {
@@ -49,7 +49,7 @@ const alumniList = [
   },
   {
     name: "Ahana Gautam",
-    designation: "Co-founder & CEO Open Secret",
+    designation: "Co-founder and CEO, Open Secret",
     imageSrc: Ahana_Gautam,
   },
   {
