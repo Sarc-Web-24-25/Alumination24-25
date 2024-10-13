f19e9529f8fd6132f789157330c8b51615f25641
Arush <arrush.narayan01@gmail.com>
2024-10-13 20:50:39 +0530

    team page photo and css

--- frontend/src/components/Home/Team3/Team3.jsx
+++ frontend/src/components/Home/Team3/Team3.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 import { AnimatedTooltip } from "./ui/animated-tooltip";
+import oc from '../photos24/thumbnail/oc.png'
+import asmp from '../photos24/thumbnail/asmp.png'
+import hda from '../photos24/thumbnail/hda.png'
+import design from '../photos24/thumbnail/design.png'
+import events from '../photos24/thumbnail/events.png'
+import marki from '../photos24/thumbnail/marki.png'
+import mpr from '../photos24/thumbnail/mpr.png'
+import ops from '../photos24/thumbnail/ops.png'
+import web from '../photos24/thumbnail/web.png'
 
 const people = [  
   {
@@ -7,63 +16,63 @@ const people = [
     name: "OC",
     id_name: "oc",
     designation: "The Explorer",
-    image: "https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80",
+    image: oc,
   },
   {
     id: 1,
     name: "ASMP",
     id_name: "asmp",
     designation: "Software Engineer",
-    image: "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+    image: asmp,
   },
   {
     id: 2,
     name: "HDA",
     id_name: "hda",
     designation: "Product Manager",
-    image: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    image: hda,
   },
   {
     id: 3,
     name: "Events",
     id_name: "events",
     designation: "Data Scientist",
-    image: "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    image: events,
   },
   {
     id: 4,
     name: "Marketing",
     id_name: "marketing",
     designation: "UX Designer",
-    image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    image: marki,
   },
   {
     id: 5,
     name: "Operations",
     id_name: "operations",
     designation: "Soap Developer",
-    image: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+    image: ops,
   },
   {
     id: 6,
     name: "Media & PR",
     id_name: "mpr",
     designation: "The Explorer",
-    image: "https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80",
+    image: mpr,
   },
   {
     id: 7,
     name: "Design",
     id_name: "design",
     designation: "Soap Developer",
-    image: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+    image: design,
   },
   {
     id: 8,
     name: "Web",
     id_name: "web",
     designation: "The Explorer",
-    image: "https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80",
+    image: web,
   },
 ];
 
