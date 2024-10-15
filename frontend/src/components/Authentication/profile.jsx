4492d395e8eca0d14b8b3e1ab5b199c292c7fd09
KhushiYadav18 <yadavkhushi00045@gmail.com>
2024-10-15 18:23:23 +0530

    department lase added

--- frontend/src/components/Authentication/profile.jsx
+++ frontend/src/components/Authentication/profile.jsx
@@ -285,6 +285,7 @@ function Profile() {
         "Center for Machine Intelligence and Data Science": "CMIDS",
         "Centre for Urban Science and Engineering": "CUSE",
         "Centre of Studies in Resources Engineering": "CSRE",
+        "Center for Liberal Education":"LASE",
         "Chemical Engineering": "Chemical Engg",
         "Chemistry": "Chemistry",
         "Civil Engineering": "Civil Engg",
