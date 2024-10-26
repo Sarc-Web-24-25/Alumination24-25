256c0a607940fd94fa4ad5d507b110379a7317ed
KhushiYadav18 <yadavkhushi00045@gmail.com>
2024-10-27 01:10:17 +0530

    time of schedule changed

--- frontend/src/components/Home/schedule.jsx
+++ frontend/src/components/Home/schedule.jsx
@@ -3,19 +3,19 @@ import "./schedule.css"; // Import your CSS file
 import CursorAnimation from "./CursorAnimation";
 function Schedule() {
   const eventsSchedule = [
-    { time: "10:00 AM - 5:00 PM", event: "Group Mentoring" },
-    { time: "10:00 AM - 5:00 PM", event: "Break The Ice " },
+    { time: "10:00 AM - 3:00 PM", event: "Group Mentoring" },
+    { time: "10:00 AM", event: "Break The Ice " },
     { time: "10:00 AM - 5:00 PM", event: "MI/GD " },
-    { time: "10:00 AM - 5:30 PM", event: "Innovation Expo " },
-    { time: "10:00 AM - 7:00 PM", event: "Hostalgia " },
-    { time: "10:30 AM - 11:30 PM", event: "Quant Workshop" },
-    { time: "11:30 AM - 12:30 PM", event: "IT/Software and Consult Workshop" },
+    { time: "11:00 AM", event: "Innovation Expo " },
+    { time: "10:00 AM - 5:00 PM", event: "Hostalgia " },
+    { time: "10:00 AM - 11:00 AM", event: "Consult Workshop" },
+    { time: "11:00 AM - 12:00 PM", event: "PM Workshop" },
+    { time: "12:00 PM - 1:00 PM", event: "Finance Workshop" },
+    { time: "1:30 PM - 2:30 PM", event: "Quant Workshop" },
     { time: "11:00 AM - 3:30 PM", event: "SARCathon Finals" },
-    { time: "1:30 PM - 2:30 PM", event: "PM Workshop" },
-    { time: "2:30 PM - 3:30 PM", event: "Finance Workshop" },
-    { time: "3:00 PM - 4:30 PM", event: "Start It Up" },
-    { time: "4:00 PM - 5:30 PM", event: "Un-Engineering" },
-    { time: "5:30 PM - 7:00 PM", event: "Trailblazers" },
+    { time: "6:00 PM", event: "Start It Up" },
+    { time: "4:00 PM", event: "Un-Engineering" },
+    { time: "2:00 PM", event: "Trailblazers" },
     { time: "7:30 PM - 9:30 PM", event: "Beyond The Horizon " },
   ];
   return (
